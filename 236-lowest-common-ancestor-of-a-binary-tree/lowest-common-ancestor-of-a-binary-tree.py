0f86b239813eb04a58129f24afb0bd4ea7b619bb
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-16 00:49:42 -0400

    Update lowest-common-ancestor-of-a-binary-tree.py

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -8,16 +8,18 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
+        ## If we reach end of the tree i.e. leaf node's children or we find one of the p or q -> return the current node
         if not root or root == p or root == q:
             return root
+
+        ## Call LCS recursively on left and right
         left_ = self.lowestCommonAncestor(root.left, p, q)
         right_ = self.lowestCommonAncestor(root.right, p, q)
 
+        ## If we find p and q on different sides => current node is lowest common ancestor
         if left_ and right_:
             return root
-        
-        if left_ or right_:
-            return left_ or right_
-        
-        return None
+
+        ## If we get both p and q on one side, return that side 
+        return left_ or right_
         
