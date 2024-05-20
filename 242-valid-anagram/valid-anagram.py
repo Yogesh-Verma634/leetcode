ffde8b89ac9f5181d9baada150a6b65be9ef4b1f
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 14:08:43 -0400

    Time: 42 ms (86.84%) | Memory: 16.8 MB (91.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        ### Hashset solution
+        count_char_s = {}
+        count_char_t = {}
+
+        for ch in s:
+            if ch not in count_char_s:
+                count_char_s[ch] = 0
+            count_char_s[ch] += 1 
+        
+        for ch in t:
+            if ch not in count_char_t:
+                count_char_t[ch] = 0
+            count_char_t[ch] += 1 
+
+        return count_char_s == count_char_t
+        ### Sorting solution, TC - N(log N), SC - O(1)
         return sorted(s) == sorted(t)
         
