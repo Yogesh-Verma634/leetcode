1c6e7094b7e9d6b4ef1ab9541e25c764b104079f
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 16:26:22 -0400

    Time: 37 ms (55.59%) | Memory: 16.7 MB (96.78%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -5,39 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self, placement,level, root, dic):
-        if(not root):
-            return
-        dic[placement].append((level, root.val))
-        self.helper(placement-1, level+1, root.left, dic)
-        self.helper(placement+1, level+1, root.right, dic)
-		
-    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
-        dic = defaultdict(list)
-        self.helper(0,0, root, dic)
-        result = []
-        for i in sorted(dic.keys()):
-            temp = []
-            for j in sorted(dic[i]):
-                temp.append(j[1])
-            result.append(temp)
-        return result
-    # def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-    #     self.vert_map = defaultdict(list)
-    #     ### DFS Implementation
-    #     def dfs(node, hd, vd):
-    #         if not node:
-    #             return
-    #         self.vert_map[hd].append((node.val, vd))
-    #         dfs(node.left, hd - 1, vd + 1)
-    #         dfs(node.right, hd + 1, vd + 1)
-    #     dfs(root, 0, 0)
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.vert_map = defaultdict(list)
+        ### DFS Implementation
+        def dfs(node, hd, vd):
+            if not node:
+                return
+            self.vert_map[hd].append((vd, node.val))
+            dfs(node.left, hd - 1, vd + 1)
+            dfs(node.right, hd + 1, vd + 1)
+        dfs(root, 0, 0)
 
-    #     vertical_order = []
-    #     for key, val in sorted(self.vert_map.items()):
-    #         val.sort(key = lambda a: (a[1], a[0]))
-    #         vertical_order.append(v for v, d in val)
-    #     return vertical_order
+        vertical_order = []
+        for key, val in sorted(self.vert_map.items()):
+            # val.sort(key = lambda a: (a[1], a[0]))
+            vertical_order.append(v[1] for v in sorted(val))
+        return vertical_order
 
         ### BFS implementation, TC - O(N) for queue, O(N Log N) for worst sorting 
         # SC - O(N) for deque, O(width) -> O(N) for worst
