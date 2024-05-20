d7a8522f81cba1d493b15d8ed0d9f5dbc64598ae
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 13:37:52 -0400

    Time: 91 ms (52.39%) | Memory: 18.3 MB (77.22%) - LeetSync

---
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,43 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        self.par = {}
+        self.rank = {}
+
+        for i in range(0, n):
+            self.par[i] = i
+            self.rank[i] = 0
+        
+        for edge1, edge2 in edges:
+            self.union(edge1, edge2)
+        
+        parent_set = set()
+        for i in range(0, n):
+            parent = self.find(i)
+            if parent not in parent_set:
+                parent_set.add(parent)
+        
+        return len(parent_set)
+    
+    # Find parent of n, with path compression.
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+
+    # Union by height / rank.
+    # Return false if already connected, true otherwise.
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += 1
+        return True
