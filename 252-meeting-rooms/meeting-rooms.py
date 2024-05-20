942b9220a5fe8ebe6bb7dd0ebf6ee3212dba71f7
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 14:03:59 -0400

    Time: 257 ms (5.11%) | Memory: 104.2 MB (5.91%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -4,16 +4,26 @@ class Solution:
         #### Cases
         # <------->
         #    <------> => Any such overlaps is False, else True
-        if not intervals:
-            return True
-        intervals.sort()
-        
-        for idx in range(1, len(intervals)):
-            prev_start, prev_end = intervals[idx-1][0], intervals[idx-1][1]
-            curr_start, curr_end = intervals[idx][0], intervals[idx][1]
+        times_occupied = set()
 
-            if curr_start < prev_end:
-                return False
+        for curr_start, curr_end in intervals:
+            for curr_time in range(curr_start, curr_end):
+                if curr_time in times_occupied:
+                    return False
+                times_occupied.add(curr_time)
+        return True
+
+        #### Sorting Solution => TC - Nlog(N), SC - O(1)
+        # if not intervals:
+        #     return True
+        # intervals.sort()
+        
+        # for idx in range(1, len(intervals)):
+        #     prev_start, prev_end = intervals[idx-1][0], intervals[idx-1][1]
+        #     curr_start, curr_end = intervals[idx][0], intervals[idx][1]
+
+        #     if curr_start < prev_end:
+        #         return False
         
         return True
 
