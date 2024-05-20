5b0ff0f10e25395592b0abdb8bd8cc1e4e67f7ea
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 13:47:40 -0400

    Time: 88 ms (69.18%) | Memory: 18.5 MB (32.78%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,22 +1,45 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        self.par = {}
-        self.rank = {}
+        
+        self.adjList = defaultdict(list)
 
-        for i in range(0, n):
-            self.par[i] = i
-            self.rank[i] = 0
+        for src, dest in edges:
+            self.adjList[src].append(dest)
+            self.adjList[dest].append(src)
         
-        for edge1, edge2 in edges:
-            self.union(edge1, edge2)
+        ## DFS Solution
+        visited = set()
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+            for neighbor in self.adjList[node]:
+                    dfs(neighbor)
+        count = 0
+        for idx in range(n):
+            if idx not in visited:
+                dfs(idx)
+                count += 1
+        return count
         
-        parent_set = set()
-        for i in range(0, n):
-            parent = self.find(i)
-            if parent not in parent_set:
-                parent_set.add(parent)
+        ### Union Find Solution => TC - O(N log N), SC - O(N) 
+        # self.par = {}
+        # self.rank = {}
+
+        # for i in range(0, n):
+        #     self.par[i] = i
+        #     self.rank[i] = 0
         
-        return len(parent_set)
+        # for edge1, edge2 in edges:
+        #     self.union(edge1, edge2)
+        
+        # parent_set = set()
+        # for i in range(0, n):
+        #     parent = self.find(i)
+        #     if parent not in parent_set:
+        #         parent_set.add(parent)
+        
+        # return len(parent_set)
     
     # Find parent of n, with path compression.
     def find(self, n):
