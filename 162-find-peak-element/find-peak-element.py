63db60449fc3f18076ed7b5b8b1f1e36e519f073
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 22:42:18 -0400

    Time: 48 ms (46.82%) | Memory: 16.7 MB (36.76%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -2,15 +2,34 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
         ### Binary Search Solution, TC - O(log n)
-        left, mid, right = 0, 0, len(nums)-1       
-        while left<right:            
-            mid = (left+right)//2
-            if nums[mid]>nums[mid+1]:
-                right = mid
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            elif mid and nums[mid] < nums[mid - 1]:
+                r = mid - 1
             else:
-                left = mid +1               
-        return left
+                return mid
+        
 
+        ### Brute Force Solution, TC - O(N)
+        # start = last = float('-inf')
+        # def isPeak(prev, curr, next):
+        #     return curr > prev and curr > next
+
+        # if len(nums) == 1:
+        #     return 0
+
+        # for idx, val in enumerate(nums):
+        #     if not idx:
+        #         if isPeak(start, val, nums[idx+1]):
+        #             return idx
+        #     if idx == len(nums) - 1:
+        #         if isPeak(nums[idx-1], val, last):
+        #             return idx
+        #     if isPeak(nums[idx-1], val, nums[idx+1]):
+        #         return idx
         
 
         ### Brute Force Solution, TC - O(N)
