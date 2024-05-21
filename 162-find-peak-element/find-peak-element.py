097cf77007d57cfe7bb5cb4a9b519ea0a2eea361
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-20 22:28:50 -0400

    Time: 52 ms (19.20%) | Memory: 16.7 MB (86.77%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,19 +1,33 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        start = last = float('-inf')
-        def isPeak(prev, curr, next):
-            return curr > prev and curr > next
+        
+        ### Binary Search Solution, TC - O(log n)
+        l, r = 0, len(nums)-1
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                l = mid + 1
+            elif mid and nums[mid] < nums[mid - 1]:
+                r = mid - 1
+            else:
+                return mid
+        
 
-        if len(nums) == 1:
-            return 0
+        ### Brute Force Solution, TC - O(N)
+        # start = last = float('-inf')
+        # def isPeak(prev, curr, next):
+        #     return curr > prev and curr > next
 
-        for idx, val in enumerate(nums):
-            if not idx:
-                if isPeak(start, val, nums[idx+1]):
-                    return idx
-            if idx == len(nums) - 1:
-                if isPeak(nums[idx-1], val, last):
-                    return idx
-            if isPeak(nums[idx-1], val, nums[idx+1]):
-                return idx
+        # if len(nums) == 1:
+        #     return 0
+
+        # for idx, val in enumerate(nums):
+        #     if not idx:
+        #         if isPeak(start, val, nums[idx+1]):
+        #             return idx
+        #     if idx == len(nums) - 1:
+        #         if isPeak(nums[idx-1], val, last):
+        #             return idx
+        #     if isPeak(nums[idx-1], val, nums[idx+1]):
+        #         return idx
         
