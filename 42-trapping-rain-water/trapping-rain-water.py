941b2be64edc8ba99f92ee79488298a4ee9a60a1
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-24 09:17:15 -0400

    Time: 114 ms (24.50%) | Memory: 18.3 MB (92.24%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -6,12 +6,13 @@ class Solution:
         for idx in range(len(height)-2, -1, -1):
             max_h = max(height[idx+1], max_h)
             right_h.append(max_h)
-        right_h = right_h[::-1]
+        # right_h = right_h[::-1]
+        total = len(height) - 1
 
         water = 0
         max_h = 0
         for idx, h in enumerate(height):
-            curr_h = min(right_h[idx], max_h) - height[idx]
+            curr_h = min(right_h[total - idx], max_h) - height[idx]
             if curr_h > 0:
                 water += curr_h
             max_h = max(max_h, h)
