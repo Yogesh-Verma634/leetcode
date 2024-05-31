314f5b1d96fe8bbf1691c9cede1d9a4b227fe561
YogeshVerma <yogeshverma.ee@gmail.com>
2024-05-31 15:54:23 -0400

    Time: 82 ms (69.51%) | Memory: 17.8 MB (35.43%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.py
+++ 1242-matrix-block-sum/matrix-block-sum.py
@@ -6,22 +6,23 @@ class Solution:
         
         ROWS = len(mat)
         COLS = len(mat[0])
-        sum = mat[:][:]
+        sum = [[0 for _ in range(COLS + 1)] for _ in range(ROWS + 1)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                sum[r+1][c+1] = mat[r][c]
 
         for r in range(ROWS):
             for c in range(COLS):
-                sum[r][c] += (sum[r][c-1] if c > 0 else 0) \
-                            + (sum[r-1][c] if r > 0 else 0) \
-                            - (sum[r-1][c-1] if r > 0 and c > 0 else 0)
+                sum[r+1][c+1] += (sum[r+1][c]) \
+                            + (sum[r][c+1]) \
+                            - (sum[r][c])
         output = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+        # print(sum)
         for r in range(ROWS):
             for c in range(COLS):
                 r1, c1 = max(r - k, 0), max(c - k, 0)
-                r2, c2 = min(r + k, ROWS - 1), min(c + k, COLS - 1) 
-                output[r][c] = sum[r2][c2] + \
-                (sum[r - k - 1][c - k - 1] if (r - k) > 0 and (c - k) > 0 else 0) - \
-                (sum[r - k - 1][c2] if (r - k) > 0 else 0) - \
-                (sum[r2][c - k - 1] if (c - k) > 0 else 0)
+                r2, c2 = min(r + k + 1, ROWS), min(c + k + 1, COLS)
+                output[r][c] = sum[r2][c2] + sum[r1][c1] - sum[r2][c1] - sum[r1][c2]
         
         return output
                 
