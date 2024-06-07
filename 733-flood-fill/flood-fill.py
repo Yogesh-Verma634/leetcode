e0710a5e3d3e06b4702c9aec8b0cc2b7783bd4b0
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-06 22:59:40 -0400

    Time: 69 ms (40.65%) | Memory: 16.6 MB (70.40%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,7 +1,7 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
-        directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        directions = [(0, 0), (0, -1), (0, 1), (1, 0), (-1, 0)]
         ROWS = len(image)
         COLS = len(image[0])
         visited = set()
@@ -13,13 +13,14 @@ class Solution:
             if (r, c) in visited:
                 return
 
-            if 0 <= r < ROWS and 0 <= c < COLS and image[r][c] == color_req:
-                image[r][c] = color
-                visited.add((r, c))
+            # if 0 <= r < ROWS and 0 <= c < COLS and image[r][c] == color_req:
+            image[r][c] = color
+            visited.add((r, c))
             
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < ROWS and 0 <= nc < COLS and image[nr][nc] == color_req:
+                if 0 <= nr < ROWS and 0 <= nc < COLS and image[nr][nc] == color_req \
+                    and (nr, nc) not in visited:
                     dfs_helper(nr, nc)
 
         dfs_helper(sr, sc)
