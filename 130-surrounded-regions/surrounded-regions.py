0ed30e7e875e68a382f4ed81ed79b098ef858e09
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-09 23:19:27 -0400

    Time: 115 ms (78.73%) | Memory: 17.9 MB (49.03%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,37 +3,38 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        if not board:
+            return
         ROWS, COLS = len(board), len(board[0])
-        visited = set()
-
+        
         def dfs_helper(r, c):
             
-            if r >= ROWS or c >= COLS:
-                return False
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or board[r][c] != 'O':
+                return
             
-            if board[r][c] == "X":
-                return True
+            board[r][c] = "B"
 
             directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if nr < 0 or nr >= ROWS or nc < 0 or nc >= COLS:
-                    return False
-                if board[nr][nc] == "O" and (nr, nc) not in visited:
-                    visited.add((nr, nc))
-                    if not dfs_helper(nr, nc):
-                        return False
-            
-            # visited = set()
-            return True
+                dfs_helper(nr, nc)
+
+        for r in range(ROWS):
+            if board[r][0] == "O":
+                dfs_helper(r, 0) # Boundary spots
+            if board[r][COLS-1] == "O":
+                dfs_helper(r, COLS-1)
+
+        for c in range(COLS):
+            if board[0][c] == "O":
+                dfs_helper(0, c) # Boundary spots
+            if board[ROWS-1][c] == "O":
+                dfs_helper(ROWS-1, c)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == "O":
-                    visited = set()
-                    if dfs_helper(r, c):
-                        board[r][c] = "X"
-                        for sr, sc in visited:
-                            board[sr][sc] = "X"
-                
+                    board[r][c] = "X"
+                elif board[r][c] == "B":
+                    board[r][c] = "O"
