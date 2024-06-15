761c9870dfaf4dcd4ba971765ed2c9dd63248348
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-15 07:03:33 -0400

    Time: 36 ms (79.43%) | Memory: 18.1 MB (24.26%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -7,26 +7,43 @@
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
         self.min_node = root.val
+        self.min_val = abs(root.val - target)
+        ## Iterative Tree Traversal
+        while root:
+            diff = abs(root.val - target)
+            if diff < self.min_val:
+                self.min_val = diff
+                self.min_node = root.val
+            elif diff == self.min_val:
+                self.min_node = min(self.min_node, root.val)
 
-
-        def tree_traversal(node, min_val):
-            if not node:
-                return min_val
-            
-            diff = abs(node.val - target)
-
-            if diff < min_val:
-                self.min_node = node.val
-                min_val = diff
-            
-            if diff == min_val:
-                self.min_node = min(node.val, self.min_node)
-
-            if node.left:
-                tree_traversal(node.left, min_val)
-
-            if node.right:
-                tree_traversal(node.right, min_val)
-
-        tree_traversal(root, abs(target - root.val))
+            if root.val < target:
+                root = root.right
+            else:
+                root = root.left
         return self.min_node
+
+
+
+        ## Recursive Tree traversal
+        # def tree_traversal(node, min_val):
+        #     if not node:
+        #         return min_val
+            
+        #     diff = abs(node.val - target)
+
+        #     if diff < min_val:
+        #         self.min_node = node.val
+        #         min_val = diff
+            
+        #     if diff == min_val:
+        #         self.min_node = min(node.val, self.min_node)
+
+        #     if node.left:
+        #         tree_traversal(node.left, min_val)
+
+        #     if node.right:
+        #         tree_traversal(node.right, min_val)
+
+        # tree_traversal(root, abs(target - root.val))
+        # return self.min_node
