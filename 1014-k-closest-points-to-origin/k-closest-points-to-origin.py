b0403f964257cc59e77a2331bc348b2bc1315fda
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-15 17:42:30 -0400

    Time: 601 ms (46.57%) | Memory: 22.9 MB (54.45%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,19 +3,12 @@ class Solution:
         res = []
         heapq.heapify(res)
 
-        def distance(x, y):
-            return math.sqrt(int(x*x + y*y))
-
         for idx, (x, y) in enumerate(points):
-            d = distance(x, y)
-            heapq.heappush(res, (-d, [x, y]))
+            d = -(x*x + y*y)
+            heapq.heappush(res, (d, [x, y]))
 
             if len(res) > k:
                 heapq.heappop(res)
-                
-        final_res = []
-        for val, point in res:
-            final_res.append(point)
 
-        return final_res
+        return [(x, y) for d, [x, y] in res]
 
