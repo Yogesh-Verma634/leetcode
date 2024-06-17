d346dcd0bdb0bd30480c641f643bd79da9738073
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-16 21:47:12 -0400

    Time: 87 ms (66.38%) | Memory: 21 MB (80.49%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,28 +4,7 @@ class Solution:
         heap = [[k, v] for k, v in count_map.items()]
         heapq.heapify(heap)
 
-        # while len(heap) == k:
-        #     heapq.heappop(heap)
         return [k for k, v in heapq.nlargest(k, heap, key=lambda e: e[1])]
-        # return []
-
-        # if len(nums) == k:
-        #     return nums
-        # reverse_count = defaultdict(list)
-
-        # for key, val in count_map.items():
-        #     reverse_count[val].append(key)
-        
-        # keys = list(sorted(reverse_count.keys(), reverse=True))
-
-        # res = []
-
-        # for key in keys:
-        #     for val in reverse_count[key]:
-        #         if len(res) == k:
-        #             return res
-        #         res.append(val)
-        # return res
 
 
             
