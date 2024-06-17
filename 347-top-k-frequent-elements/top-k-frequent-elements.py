ceb73177de21da909d6f382ff1066475294f38c9
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-16 21:46:58 -0400

    Time: 88 ms (60.92%) | Memory: 21.1 MB (51.39%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,23 +1,31 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count_map = Counter(nums)
-        if len(nums) == k:
-            return nums
-        reverse_count = defaultdict(list)
+        heap = [[k, v] for k, v in count_map.items()]
+        heapq.heapify(heap)
 
-        for key, val in count_map.items():
-            reverse_count[val].append(key)
+        # while len(heap) == k:
+        #     heapq.heappop(heap)
+        return [k for k, v in heapq.nlargest(k, heap, key=lambda e: e[1])]
+        # return []
+
+        # if len(nums) == k:
+        #     return nums
+        # reverse_count = defaultdict(list)
+
+        # for key, val in count_map.items():
+        #     reverse_count[val].append(key)
         
-        keys = list(sorted(reverse_count.keys(), reverse=True))
+        # keys = list(sorted(reverse_count.keys(), reverse=True))
 
-        res = []
+        # res = []
 
-        for key in keys:
-            for val in reverse_count[key]:
-                if len(res) == k:
-                    return res
-                res.append(val)
-        return res
+        # for key in keys:
+        #     for val in reverse_count[key]:
+        #         if len(res) == k:
+        #             return res
+        #         res.append(val)
+        # return res
 
 
             
