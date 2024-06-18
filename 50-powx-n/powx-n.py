477f58d82a899237f19627994a9e47c16662beca
YogeshVerma <yogeshverma.ee@gmail.com>
2024-06-17 23:04:48 -0400

    Time: 42 ms (12.29%) | Memory: 16.5 MB (54.45%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -10,7 +10,7 @@ class Solution:
             return pow(1/x, -n)
         
         if n%2 == 0:
-            return pow(x*x, n/2)
+            return pow(x*x, n//2)
         
         else:
-            return x * pow(x*x, (n-1)/2)
+            return x * pow(x*x, (n-1)//2)
